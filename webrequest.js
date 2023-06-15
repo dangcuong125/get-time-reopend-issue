ce80811d49db7a221e8f0539cceb51b5e25f88a5
Cuong Dang <dangcuong1225@gmail.com>
2023-06-15 12:03:01 +0700

    check

--- webrequest.js
+++ webrequest.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 async function webrequest(url, headers) {
-  const config = {
+  let config = {
     method: "get",
     maxBodyLength: Infinity,
     url: "https://api.github.com/repos/bilisoftware/traphaco-manage/issues/133/timeline",
@@ -9,12 +9,15 @@ async function webrequest(url, headers) {
       Authorization: "token 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",
     },
   };
-  try {
-    const response = await axios.request(config);
-    return response;
-  } catch (error) {
-    console.error(error);
-  }
+
+  axios
+    .request(config)
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 module.exports = webrequest;
