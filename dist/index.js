86e031f7193194f50c47ea408d60b1f153d37cfe
Cuong Dang <dangcuong1225@gmail.com>
2023-06-15 13:49:07 +0700

    check

--- dist/index.js
+++ dist/index.js
@@ -6157,17 +6157,42 @@ exports["default"] = _default;
 const axios = __nccwpck_require__(9109);
 
 async function webrequest(url, headers) {
-  const config = {
-    headers: {
-      Authorization: "token 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",
-    },
+  // const config = {
+  //   headers: {
+  //     Authorization: "token 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",
+  //   },
+  // };
+  // try {
+  //   const response = await axios.get(url, config);
+  //   return response;
+  // } catch (error) {
+  //   console.error(error);
+  // }
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "Authorization",
+    "token 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"
+  );
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
   };
+
   try {
-    const response = await axios.get(url, config);
-    return response;
+    const res = await fetch(
+      "https://api.github.com/repos/bilisoftware/traphaco-manage/issues/133/timeline",
+      requestOptions
+    );
+    return res;
   } catch (error) {
     console.error(error);
   }
+
+  // .then((response) => response.text())
+  // .then((result) => console.log(result))
+  // .catch((error) => console.log("error", error));
 }
 
 module.exports = webrequest;
--- webrequest.js
+++ webrequest.js
@@ -1,17 +1,42 @@
 const axios = require("axios");
 
 async function webrequest(url, headers) {
-  const config = {
-    headers: {
-      Authorization: "token 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",
-    },
+  // const config = {
+  //   headers: {
+  //     Authorization: "token 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",
+  //   },
+  // };
+  // try {
+  //   const response = await axios.get(url, config);
+  //   return response;
+  // } catch (error) {
+  //   console.error(error);
+  // }
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "Authorization",
+    "token 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"
+  );
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
   };
+
   try {
-    const response = await axios.get(url, config);
-    return response;
+    const res = await fetch(
+      "https://api.github.com/repos/bilisoftware/traphaco-manage/issues/133/timeline",
+      requestOptions
+    );
+    return res;
   } catch (error) {
     console.error(error);
   }
+
+  // .then((response) => response.text())
+  // .then((result) => console.log(result))
+  // .catch((error) => console.log("error", error));
 }
 
 module.exports = webrequest;
