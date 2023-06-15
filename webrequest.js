49e2b01ce64d6840d1f263acf427639b47dd8af4
Cuong Dang <dangcuong1225@gmail.com>
2023-06-15 13:51:50 +0700

    check

--- webrequest.js
+++ webrequest.js
@@ -1,42 +1,21 @@
 const axios = require("axios");
 
 async function webrequest(url, headers) {
-  // const config = {
-  //   headers: {
-  //     Authorization: "token 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",
-  //   },
-  // };
-  // try {
-  //   const response = await axios.get(url, config);
-  //   return response;
-  // } catch (error) {
-  //   console.error(error);
-  // }
-  var myHeaders = new Headers();
-  myHeaders.append(
-    "Authorization",
-    "token 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"
-  );
-
-  var requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "https://api.github.com/repos/bilisoftware/traphaco-manage/issues/133/timeline",
+    headers: {
+      Authorization: "token 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",
+    },
   };
 
   try {
-    const res = await fetch(
-      "https://api.github.com/repos/bilisoftware/traphaco-manage/issues/133/timeline",
-      requestOptions
-    );
+    const res = await axios.request(config);
     return res;
   } catch (error) {
     console.error(error);
   }
-
-  // .then((response) => response.text())
-  // .then((result) => console.log(result))
-  // .catch((error) => console.log("error", error));
 }
 
 module.exports = webrequest;
