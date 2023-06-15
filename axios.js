47a2668b48702c3e4068500a14e2852536b1f0a4
Cuong Dang <dangcuong1225@gmail.com>
2023-06-15 14:02:49 +0700

    check

--- axios.js
+++ axios.js
@@ -7,13 +7,12 @@ const axiosClient = axios.create({
     "https://api.github.com/repos/bilisoftware/traphaco-manage/issues/133/timeline",
   headers: {
     "Content-Type": "application/json",
-    Accept: "Application/json",
   },
 });
 
 axiosClient.interceptors.request.use(async (config) => {
   try {
-    config.headers.Authorization = `token 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`;
+    config.headers.Authorization = `token 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`;
   } catch (e) {
     console.log(e);
   }
