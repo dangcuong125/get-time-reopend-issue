129a3799d9bd275c7877ce3857cabd23485f2f23
Cuong Dang <dangcuong1225@gmail.com>
2023-06-15 14:07:48 +0700

    check

--- webrequest.js
+++ webrequest.js
@@ -1,8 +1,16 @@
-const { default: axiosClient } = require("./axios");
+const axios = require("axios");
 
 async function webrequest(url, headers) {
+  let config = {
+    method: "get",
+    url: url,
+    headers: {
+      ...headers,
+    },
+  };
+
   try {
-    const res = await axiosClient.get("/");
+    const res = await axios.request(config);
     return res;
   } catch (error) {
     console.error(error);
